73856c2d8ef66bf009cdec8557a04727296cbe1f
Agustin <agualvila@gmail.com>
2022-01-28 20:14:49 -0300

    fix parse

--- src/db/firebaseCredentials.js
+++ src/db/firebaseCredentials.js
@@ -1,8 +1,10 @@
+const pKey = process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n');
+
 const fbCredentials = {
   type: 'service_account',
   project_id: 'simple-todo-ced11',
   private_key_id: process.env.FB_PRIVATE_KEY_ID,
-  private_key: process.env.FB_PRIVATE_KEY,
+  private_key: pKey,
   client_email:
     'firebase-adminsdk-p7qzl@simple-todo-ced11.iam.gserviceaccount.com',
   client_id: '103158606984412285251',
